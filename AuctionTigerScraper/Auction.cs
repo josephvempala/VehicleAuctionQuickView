c9af64855bba60f78f19a944e5b9e238d65a73b1
Joseph Boby <josephvempala@gmail.com>
2021-04-28 18:45:59 +0530

    added web Api

--- AuctionTigerScraper/Auction.cs
+++ AuctionTigerScraper/Auction.cs
@@ -1,13 +1,20 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace AuctionTigerScraper
 {
     public class Auction
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Link { get; set; }
         public string Description { get; set; }
         public DocumentData[] DocumentData { get; set; }
         public List<Vehicle> Vehicles { get; set; }
+
+        public bool Equals(Auction obj)
+        {
+            return obj.Description.GetHashCode() == Description.GetHashCode();
+        }
     }
 }
--- AuctionTigerScraper/Program.cs
+++ AuctionTigerScraper/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Linq;
 using System.IO;
 using System;
 
@@ -12,39 +13,45 @@ namespace AuctionTigerScraper
         #region private
         private string username;
         private string password;
+        private bool currentlyChecking = false;
         private Browser browser;
         private Page page;
-        private List<Auction> Auctions;
-        private List<Vehicle> DesirableVehicles;
+        public List<Auction> Auctions;
+        public List<Vehicle> Vehicles = new();
         private async Task DeserializeVehicleInfo()
         {
             await Task.Run(() =>
             {
-                if (Auctions is not null)
+                if (Auctions is null)
                 {
-                    foreach (Auction Auction in Auctions)
+                    return;
+                }
+                List<Auction> ToBeRemoved = new();
+                foreach (Auction Auction in Auctions)
+                {
+                    MatchCollection matches = null;
+                    Auction.Vehicles = new List<Vehicle>();
+                    matches = Regex.Matches(Regex.Replace(Auction.Description, @"\n", " "), @"(?>(?<AucName>.+?)?(?>[\(]?(\d{1,2})[\)]?) ?(?<CarName>.+?)(?<Number>(?>[A-Z]{2}[- ]*?[A-Z\d]{1,2}[- ]*?[A-Z\d]{1,2}[- ]*?[\d]{4} ))(?<Fuel>(?>Diesel|Petrol|CNG|0)).*?(?<Year>\d{4}) (?<Address>.+?)(?:(?>Ownership( status)?)) ?(?<Status>(?>\d))? ?(?:(?>Remark))?(?<Remarks>.+?)?(?<Reference>(?>MOT\d{8})))", RegexOptions.IgnoreCase | RegexOptions.ExplicitCapture, TimeSpan.FromMilliseconds(1000));
+                    try
                     {
-                        MatchCollection matches = null;
-                        Auction.Vehicles = new List<Vehicle>();
-                        try
-                        {
-                            matches = Regex.Matches(Regex.Replace(Auction.Description, @"\n", " "), @"(?>(?<AucName>.+?)?(?>[\(]?(\d{1,2})[\)]?) ?(?<CarName>.+?)(?<Number>(?>[A-Z]{2}[- ]*?[A-Z\d]{1,2}[- ]*?[A-Z\d]{1,2}[- ]*?[\d]{4} ))(?<Fuel>(?>Diesel|Petrol|CNG|0)).*?(?<Year>\d{4}) (?<Address>.+?)(?:(?>Ownership( status)?)) ?(?<Status>(?>\d))? ?(?:(?>Remark))?(?<Remarks>.+?)?(?<Reference>(?>MOT\d{8})))", RegexOptions.IgnoreCase | RegexOptions.ExplicitCapture, TimeSpan.FromMilliseconds(100));
-                        }
-                        catch
-                        {
-                            Auctions.Remove(Auction);
-                            continue;
-                        }
                         foreach (Match match in matches)
                         {
                             if (match.Groups["AucName"].Success is true && match.Groups["AucName"].Value is not " ")
                             {
                                 Auction.Name = match.Groups["AucName"].Value;
                             }
-                            Auction.Vehicles.Add(new Vehicle(Auction, match.Groups["CarName"].Value, match.Groups["Number"].Value, match.Groups["Fuel"].Value, match.Groups["Year"].Value, match.Groups["Address"].Value, match.Groups["Status"].Value, match.Groups["Remarks"].Value, match.Groups["Reference"].Value));
+                            var vehicle = new Vehicle(Guid.NewGuid(), Auction, match.Groups["CarName"].Value, match.Groups["Number"].Value, match.Groups["Fuel"].Value, match.Groups["Year"].Value, match.Groups["Address"].Value, Auction.Id, match.Groups["Status"].Value, match.Groups["Remarks"].Value, match.Groups["Reference"].Value);
+                            Auction.Vehicles.Add(vehicle);
+                            Vehicles.Add(vehicle);
                         }
                     }
+                    catch
+                    {
+                        ToBeRemoved.Add(Auction);
+                        continue;
+                    }
                 }
+                ToBeRemoved.ForEach(item => { Auctions.Remove(item); });
             });
         }
         private async Task LoginAsync()
@@ -54,31 +61,15 @@ namespace AuctionTigerScraper
             await page.TypeAsync("#j_password", password);
             await page.ClickAsync("button.pull-left.blue-button-small");
         }
-        private async Task LoadAuctionsInfoAsync()
+        private async Task<List<Auction>> GetAuctionsInfoAsync()
         {
             await CheckSessionExpire();
             await page.WaitForSelectorAsync(".details-box");
-            Auctions = await page.EvaluateExpressionAsync<List<Auction>>(@"Array.from(document.getElementsByClassName('details-box')).map(box=>{return{Link:box.firstElementChild.firstElementChild.href,Description:box.firstElementChild.firstElementChild.text}});");
+            var auctions = await page.EvaluateExpressionAsync<List<Auction>>(@"Array.from(document.getElementsByClassName('details-box')).map(box=>{return{Link:box.firstElementChild.firstElementChild.href,Description:box.firstElementChild.firstElementChild.text}});");
+            auctions.ForEach(item => { item.Id = Guid.NewGuid(); });
+            return auctions;
         }
-        private async Task FindDesirableVehiclesAsync(IEnumerable<string> DesirableVehicleNames)
-        {
-            await CheckSessionExpire();
-            await Task.Run(() =>
-            {
-                List<Vehicle> desirablevehicles = new List<Vehicle>();
-                foreach(Auction auction in Auctions)
-                foreach (Vehicle vehicle in auction.Vehicles)
-                {
-                    foreach (string car in DesirableVehicleNames)
-                        if (vehicle.Name.ToUpper().Contains(car.ToUpper()))
-                        {
-                            desirablevehicles.Add(vehicle);
-                        }
-                }
-                DesirableVehicles = desirablevehicles;
-            });
-        }
-        private async Task<List<string>> GetDesirableVehiclesPicturesAsync()
+        private async Task<List<string>> GetDesirableVehiclesPicturesAsync(IEnumerable<Vehicle> DesirableVehicles)
         {
             await CheckSessionExpire();
             var tempdir = Path.GetTempPath();
@@ -105,6 +96,7 @@ namespace AuctionTigerScraper
             await page.GoToAsync(auction.Link);
             await page.WaitForSelectorAsync("#auctionNotice > div > table > tbody > tr:nth-child(3) > td:nth-child(7) > a");
             auction.DocumentData = await page.EvaluateExpressionAsync<DocumentData[]>("Array.from(Array.from(document.querySelectorAll('.table-border'))[1].querySelectorAll('tr')).slice(2).map((i)=>{return{DocName:i.outerText,DownloadLink:i.lastElementChild.firstElementChild.href.slice(37)}});");
+            await page.CloseAsync();
         }
         private async Task<string> DownloadPictures(Vehicle vehicle, DownloadManager downloadManager)
         {
@@ -135,8 +127,32 @@ namespace AuctionTigerScraper
                 await LoginAsync();
             }
         }
+        private async Task WatchForAuctions(Page page)
+        {
+            while(true)
+            {
+                await Task.Delay(new TimeSpan(0, 5, 0));
+                await CheckForChanges();
+            }
+        }
         #endregion
         #region public
+        public async Task CheckForChanges()
+        {
+            if (currentlyChecking)
+                return;
+            currentlyChecking = true;
+            await page.ReloadAsync();
+            await CheckSessionExpire();
+            var newAuctions = await GetAuctionsInfoAsync();
+            if (!newAuctions.CompareDescription(Auctions))
+            {
+                Auctions = newAuctions;
+                Vehicles = new();
+                await Task.WhenAll(GetAllAuctionDocuments(), DeserializeVehicleInfo());
+            }
+            currentlyChecking = false;
+        }
         public async Task InitializeScraperAsync(string username, string password)
         {
             this.username = username;
@@ -149,16 +165,24 @@ namespace AuctionTigerScraper
             page = await browser.NewPageAsync();
             await page.GoToAsync("https://icicilombard.procuretiger.com/");
             await LoginAsync();
-            await LoadAuctionsInfoAsync();
+            Auctions = await GetAuctionsInfoAsync();
+            WatchForAuctions(page);
             await Task.WhenAll(GetAllAuctionDocuments(), DeserializeVehicleInfo());
         }
-        #endregion
-        public static async Task Main(string[] args)
+        public async Task<List<Vehicle>> GetDesirableVehiclesAsync(IEnumerable<string> DesirableVehicleNames)
         {
-            AuctionScraper p = new AuctionScraper();
-            await p.InitializeScraperAsync("Karthikeyacarz@gmail.com", "Subhash@123");
-            await p.FindDesirableVehiclesAsync(new string[] {"kia"});
-            await p.browser.CloseAsync();
+            await CheckSessionExpire();
+            List<Vehicle> desirablevehicles = new List<Vehicle>();
+            foreach (Vehicle vehicle in Vehicles)
+            {
+                foreach (string car in DesirableVehicleNames)
+                    if (vehicle.Name.ToUpper().Contains(car.ToUpper()))
+                    {
+                        desirablevehicles.Add(vehicle);
+                    }
+            }
+            return desirablevehicles;
         }
+        #endregion
     }
 }
