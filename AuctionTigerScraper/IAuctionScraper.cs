a096bbc603c3a4e90c95d28b5c7df3dd61b84f9f
Joseph Boby <josephvempala@gmail.com>
2021-05-31 09:21:51 +0530

    Addded Cancellation Tokens

--- AuctionTigerScraper/IAuctionScraper.cs
+++ AuctionTigerScraper/IAuctionScraper.cs
@@ -6,9 +6,9 @@ namespace AuctionTigerScraper
 {
     public interface IAuctionScraper
     {
-        Task<bool> CheckForChangesAsync(CancellationToken cancellationToken);
+        Task<bool> CheckForChangesAsync(CancellationToken? cancellationToken = null);
         ValueTask DisposeAsync();
-        Task InitializeScraperAsync(ScraperOptions scraperOptions, CancellationToken cancellationToken);
+        Task InitializeScraperAsync(ScraperOptions scraperOptions, CancellationToken? cancellationToken = null);
         void ListenForChanges(Action<Vehicle[], Vehicle[]> toBeCalledOnChanges);
         void StopListeningForChanges(Action<Vehicle[], Vehicle[]> toBeRemoved);
     }
